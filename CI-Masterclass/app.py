4872f9c9136d31938f23421495dd7ac17c85feb9
Gautam Mishra <g20autammishra1234@gmail.com>
2026-06-22 15:01:14 +0530

    changes app.py code

--- CI-Masterclass/app.py
+++ CI-Masterclass/app.py
@@ -8,11 +8,20 @@ st.write("Enter a number to calculator its sq., cube, and fifth power.")
 n = st.number_input("Enter an integer", value=1, step=1)
 
 # calculate results
-sq = n ** 2
-cube = n ** 3 
-fifth_power = n ** 5
+# sq = n ** 2
+# cube = n ** 3 
+# fifth_power = n ** 5
+
+def sq(n):
+    return n**2
+
+def cube(n):
+    return n**3
+
+def fifth_power(n):
+    return n**5
 
 # Display results
-st.write(f"The Sq. of {n} is: {sq}")
-st.write(f"The cube of {n} is: {cube}")
-st.write(f"The fifth power of {n} is: {fifth_power}")
+st.write(f"The Sq. of {n} is: {sq(n)}")
+st.write(f"The cube of {n} is: {cube(n)}")
+st.write(f"The fifth power of {n} is: {fifth_power(n)}")
